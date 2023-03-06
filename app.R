2e86e003d418915c400681cef91880e974e59835
Stacy Read <sread@ivaalsolutions.com>
2023-03-06 12:37:52 -0600

    Removed unused packages

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
-library(plotly)
+#library(plotly)
 
 df <- read.csv("rockets.csv")
 
--- charts.R
+++ charts.R
@@ -1,3 +1,6 @@
+# This is just a scratch file to play around with charts before
+# adding them to the app
+
 library(dplyr)
 library(ggplot2)
 
@@ -16,23 +19,3 @@ ggplot(df, aes(x=Year, fill=Status.Mission)) +
   geom_rug() +
   scale_y_continuous(breaks = NULL) +
   theme(axis.title.y = element_blank())
-
-
-df %>% ggvis(~Year, ~Company.Name) %>%
-  layer_points(size := 50, size.hover := 200,
-               fillOpacity := 0.2, fillOpacity.hover := 0.5,
-               stroke = ~Status.Mission) %>%
-  add_axis("y", title = "Company") %>%
-  add_axis("x", title = "Year") %>%
-  scale_nominal("stroke", domain = c(statuses),
-              range = c("blue", "red", "orange", "pink"))
-
-
-df %>% ggvis(~Year, ~Country) %>%
-  layer_points(size := 50, size.hover := 200,
-               fillOpacity := 0.2, fillOpacity.hover := 0.5,
-               stroke = ~Status.Mission) %>%
-  add_axis("y", title = "Country") %>%
-  add_axis("x", title = "Year") %>%
-  scale_nominal("stroke", domain = c(statuses),
-                range = c("blue", "red", "orange", "pink"))
