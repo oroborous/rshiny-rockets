f3832657e9c855dc36dcb44d5bf4b427d3de7b32
Stacy Read <sread@ivaalsolutions.com>
2023-03-08 10:55:23 -0600

    Add pickerinput for companies

--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(shinyWidgets)
 
 df <- read.csv("rockets.csv")
 
@@ -18,7 +19,9 @@ ui <- fluidPage(
       sliderInput("years", "Years", minYear, maxYear,
         c(minYear, maxYear), step = 1, sep = ""),
       hr(),
-      selectInput("mfr", "Manufacturer", c(companies))
+      pickerInput("mfr", "Manufacturer", c(companies),
+                  options = list('actions-box' = TRUE),
+                  multiple = TRUE)
     ),
     
     
@@ -29,25 +32,25 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
-  observe({
-    df.filtered <- df %>% filter(
+  df.filter1 <- reactive({
+    df %>% filter(
         Year >= input$years[1] &
         Year <= input$years[2] &
         if (input$country == "ALL") TRUE else Country == input$country
     )
+  })
+  
+
+  observeEvent(input$country, {
     
-    updateSelectInput(session, "mfr",
-                      choices = c(unique(df.filtered$Company.Name)))
+    updatePickerInput(session = session, inputId = "mfr",
+                      choices = sort(unique(df.filter1()$Company.Name)),
+                      selected = sort(unique(df.filter1()$Company.Name)))
   })
   
   output$distPlot <- renderPlot({
     ggplot(
-      df %>% filter(
-          Year >= input$years[1] &
-          Year <= input$years[2] &
-          Company.Name == input$mfr &
-          if (input$country == "ALL") TRUE else Country == input$country
-      ),
+      df.filter1() %>% filter(Company.Name %in% input$mfr),
       aes(
         x = Year,
         fill = Status.Mission
@@ -60,12 +63,9 @@ server <- function(input, output, session) {
       theme(axis.title.y = element_blank())
   })
   
-  output$table <- renderDataTable({df %>% filter(
-    Year >= input$years[1] &
-      Year <= input$years[2] &
-      Company.Name == input$mfr &
-      if (input$country == "ALL") TRUE else Country == input$country
-  )}, options=list(columnDefs = list(list(visible=FALSE, targets=c(4, 5, 6, 8, 9)))))
+  output$table <- renderDataTable({
+    df.filter1() %>% filter(Company.Name %in% input$mfr)
+    }, options=list(columnDefs = list(list(visible=FALSE, targets=c(4, 5, 6, 8, 9)))))
   
 }
 
